168edf198ba4f534a5e818bfe774e1ed8f9a04e1
Ignjat Jokanovic <ignjat.jokanovic.311.16@ict.edu.rs>
2019-04-21 21:23:08 +0200

    finished layout and restrictions for insert

--- js/custom.js
+++ js/custom.js
@@ -13,4 +13,66 @@ $(document).ready(function () {
         $('.logged-out').hide();
     }
 
+    $("#image").on('change', function() {
+        var reader = new FileReader();
+        reader.onload = function(){
+            $("#preview").attr('src', reader.result).show();
+        };
+        reader.readAsDataURL($("#image").get(0).files[0]);   
+    });
+
+    //article insert
+    $('#insert').on('click', function(){
+        var title = $('#title').val();
+        var image = $("#preview").attr('src');
+        var body = quill.root.innerHTML;
+        var errors = [];
+        if(title == '')
+        {
+            errors.push("<div class='alert alert-danger'>Title field is required</div>");
+        }
+        if(image == '')
+        {
+            errors.push("<div class='alert alert-danger'>Image field is required</div>");
+        }
+        if(body == '<p><br></p>')
+        {
+            errors.push("<div class='alert alert-danger'>Body field is required</div>");
+        }
+        if(errors.length == 0)
+        {
+            const article = {
+                "title": title,
+                "image": image,
+                "body": body.replace(/"/g, "'")
+            }
+            $.ajax({
+                type: "POST",
+                url: "url",
+                data: article,
+                dataType: "application/json",
+                success: function (response)
+                {
+                    
+                },
+                error: function(response)
+                {
+
+                }
+            });
+        }
+        else
+        {
+            var text = '';
+            $.each(errors, function(key, message){
+                text += message;
+            });
+            $('#feedback_insert').html(text);
+        }
+        
+    });
+
+    
+
+
 });
