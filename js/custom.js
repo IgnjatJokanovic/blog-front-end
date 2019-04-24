bb1737cf3f715cc7b80f0d7f502a980c8854f321
Ignjat Jokanovic <ignjat.jokanovic.311.16@ict.edu.rs>
2019-04-24 16:23:27 +0200

    Added login, logout and fixed article insert

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,12 @@
 $(document).ready(function () {
 
     //navigation restriction
-    var user = localStorage.getItem('user');
+    var user = localStorage.getItem('token');
     if(user == null)
     {
         $('.logged-in').hide();
         $('.logged-out').show();
+        $('#page_insert_article').hide();
     }
     else
     {
@@ -41,23 +42,36 @@ $(document).ready(function () {
         }
         if(errors.length == 0)
         {
-            const article = {
-                "title": title,
-                "image": image,
-                "body": body.replace(/"/g, "'")
-            }
             $.ajax({
                 type: "POST",
                 url: URL + 'article/create',
-                data: article,
-                dataType: "application/json",
-                success: function (response)
-                {
-                    console.log(response);
+                data: {
+                        "title": title,
+                        "image": image,
+                        "body": body.replace(/"/g, "'"),
+                        "token": localStorage.getItem('token')
                 },
-                error: function(response)
+                dataType: "application/json",
+                complete: function (response)
                 {
-                   alert(response);
+                    if(response.status == 201)
+                    {
+                        var feedback = JSON.parse(response.responseText);
+                        $('#feedback_insert').html(`<div class='alert alert-success text-center'>${feedback.message}</div>`);
+                        $('html, body').scrollTop($("#feedback_insert").scrollTop() + 10);
+                        
+                    }
+                    else
+                    {
+                        var text = '';
+                        var errors = JSON.parse(data.responseText);
+                        $.each(errors.messages, function(key, value) {
+                            text += `<div class='alert alert-danger text-center'>${value}</div>`;
+                        });
+                        $('#feedback_insert').html(text);
+                        $('html, body').scrollTop($("#feedback_insert").scrollTop() + 10);
+                        
+                    }
                 }
             });
         }
@@ -71,6 +85,74 @@ $(document).ready(function () {
         }
         
     });
+
+    $('#login').on('click', function(){
+        var email = $('#email').val();
+        var password = $('#password').val();
+        var regex = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+        var errors = [];
+        if(email == '')
+        {
+            errors.push("<div class='alert alert-danger'>Email field is required</div>");
+        }
+        if(password == '')
+        {
+            errors.push("<div class='alert alert-danger'>Password field is required</div>");
+        }
+        if(email != '' && !email.match(regex))
+        {
+            errors.push("<div class='alert alert-danger'>Email field must be a valid email address</div>");
+        }
+        if(errors.length == 0)
+        {
+            var user = {
+                "email": email,
+                "password": password
+            }
+
+            $.ajax({
+                type: "POST",
+                url: URL + "user/login",
+                data: user,
+                dataType: "application/json",
+                complete: function (data)
+                {
+                    if(data.status == 200)
+                    {
+                        var jsonToken = JSON.parse(data.responseText);
+                        localStorage.setItem('token', jsonToken.token);
+                        $('#loginModal').hide();
+                        location.reload();
+                       
+                    }
+                    else
+                    {
+                        var text = '';
+                        var errors = JSON.parse(data.responseText);
+                        $.each(errors.messages, function(key, value) {
+                            text += `<div class='alert alert-danger text-center'>${value}</div>`;
+                        });
+                        $('#feedback_login').html(text);
+
+                    }
+                }
+            });
+
+        }
+        else
+        {
+            var text = '';
+            $.each(errors, function(key, message){
+                text += message;
+            });
+            $('#feedback_login').html(text);
+        }
+    });
+
+    $('#logout').on('click', function(){
+        localStorage.removeItem('token');
+        location.reload();
+    })
 
     
 
